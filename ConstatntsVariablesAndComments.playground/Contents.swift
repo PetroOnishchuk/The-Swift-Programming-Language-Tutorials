0f564dc58475fa6a1a09dff20df98436a0e83faa
PetroOnishchuk <petroonishchuk@icloud.com>
2020-06-05 16:19:23 +0300

    Update

--- ConstatntsVariablesAndComments.playground/Contents.swift
+++ ConstatntsVariablesAndComments.playground/Contents.swift
@@ -1,3 +1,46 @@
+//
+//  Apple Swift version 5.2.4
+//  Xcode Version 11.5 beta 2 (11N605f)
+//
+//  Created by Petro Onishchuk on Jun/03/20.
+//  Copyright © 2020 Petro Onishchuk. All rights reserved.
+//
+//  The Swift Programming Language.
+//  Topic: Constants, Variables and Comments.
+//
+
 import UIKit
 
-var str = "Hello, playground"
+// 1.Comments
+
+// Single line comment
+
+
+/*
+ First line
+ Second line
+ /*
+ Nested line
+ Nested line
+ */
+ */
+
+// 2.Constants and Variables
+
+
+let numberOfMonths = 12
+//numberOfMonths = 10
+
+var nameOFMonths = "January"
+nameOFMonths = "May"
+
+
+let age: Int
+age = 10
+
+var firstName: String = "Ron"
+
+var isConnected: Bool = true
+isConnected = false
+
+// 3.Type Annotations
