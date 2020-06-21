c1862280b30bcc74296e405a8f7332a83576a932
PetroOnishchuk <petroonishchuk@icloud.com>
2020-06-21 21:57:49 +0300

    Topic: Integers and Floating-Point Numbers. Completed

---
+++ IntegersAndFloating-PointNumbers.playground/Contents.swift
@@ -0,0 +1,53 @@
+
+//  Apple Swift version 5.2.4
+//  Version 11.6 beta (11N700h)
+//
+//  Created by Petro Onishchuk on Jun/20/20.
+//  Copyright © 2020 Petro Onishchuk. All rights reserved.
+//
+//  The Swift Programming Language.
+//  Topic: Integers and Floating-Point Numbers.
+
+
+
+import UIKit
+
+// Integer
+
+var firstIntExample: Int = 20
+
+var secondIntExample = -15
+secondIntExample = 25
+
+
+// Max and Min Integer Value
+
+let maxIntValue: Int = Int.max
+
+let minIntValue: Int = Int.min
+
+
+//Integer mo readable
+
+let firstBigNumber: Int = 500_000_000
+
+let secondBigNumber: Int = 000_500_000
+
+
+// Floating-Point Numbers
+
+let firstPointNumber: Double = 10.5
+
+let secondPointNumber: Float = 20.4
+
+
+
+// Types of Floating-Point Numbers
+
+let firstDoubleNumber: Double = 5.123456789
+
+let firstFloatNumber: Float = 5.123456789
+
+
+var secondDoubleNumber = 10.20
+secondDoubleNumber = 10.5
